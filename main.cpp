78758167c6513a1300fd3b496e8f133a13236bbe
SoWiEee <kawiisowieee@gmail.com>
2025-10-29 23:34:30 +0800

    Add GLM library

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 // vertex shader
 const char* vertexShaderSource = R"glsl(
